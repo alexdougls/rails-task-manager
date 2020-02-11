076c7101b580f170b4ec786fce00a4730e2b7361
Alexander Douglas <alexdouglas@hotmail.fr>
2020-02-11 14:24:51 +0100

    refactored task manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tasks
+  # get 'tasks/new', to: 'tasks#new'
+  # patch 'tasks/:id', to: 'tasks#update'
+  # get 'tasks', to: 'tasks#index'
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :task_edit
+  # delete 'tasks/:id', to: 'tasks#destroy'
 end
